2f6d95ca8e4e8b556510ae490d5ec107bfbd0e2a
wesin <wesinlove@hotmail.com>
2016-01-05 14:17:29 +0800

    Initial Commit

---
+++ WSStatusChoosenControl/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WSStatusChoosenControl
+//
+//  Created by 何文新 on 16/1/5.
+//  Copyright © 2016年 wesin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
